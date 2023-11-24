27b3a9c21451193638cbcf9a1260167912bcbaee
robonexx <robertwagar@gmail.com>
2023-11-24 10:39:30 +0100

    header tests

---
+++ src/__tests__/Header.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import Header from '../components/header/Header';
+
+describe('Header', () => {
+  it('renders header with the correct text', () => {
+    render(<Header />);
+
+    const headerElement = screen.getByTestId('header');
+    expect(headerElement).toBeInTheDocument();
+
+    const h2Element = screen.getByText(/Demografic age survey/i);
+    expect(h2Element).toBeInTheDocument();
+  });
+});
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import './header.css'
+import React from 'react';
+import './header.css';
 
 const Header = () => {
   return (
-    <header data-testid='header'><h2>Demografic age survey</h2></header>
-  )
-}
+    <header data-testid='header'>
+      <h2>Demografic age survey</h2>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
